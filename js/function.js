10f130856cb8a842b79e5ecf015a70fbaf31dd5e
Max Martin <maxmartin360@gmail.com>
2015-05-19 21:14:51 -0400

    fixed js

--- js/function.js
+++ js/function.js
@@ -1,19 +1,42 @@
-/*makes enter keystroke work as a submit button for input field*/
-function getItem() {
-	$('#add-items').keydown(function (enter) {
-		if (enter.keyCode == 13) {
-			postItem();
-		}
+$(document).ready(function () {
+//validate the box, if nothing entered show error message
+
+//add value to list
+	$('#add-items').keydown(function(e) {
+		
+		if (e.which == 13) {
+			$('#list-area').prepend('<li class="item"><span id="cross-off"></span>'+ $('#add-items').val() + '<span id="delete"></span></li>')
+			$('#add-items').val('');
+		} else {
+			if ($('#list-area').val().length == 0 );
+			alert('please enter a value');
+		};
+
 	});
-}
+//Change class when user clicks done button
+	$('ul').on('click','#cross-off', function() {
+		alert('this is being clicked')
+		$(this).closest('li').css({
+			'text-decoration': 'line-through',
+			'color':'#898989',
+		});
+		//$('#cross-off').css({
+		//'background-url''(http://i.imgur.com/dbRCAoT.png)',
+		//});
+		
+	});
+	//remove li when user hits x
+	$('ul').on('click','#delete', function() {
+		alert('you clicked delete')
+		$(this).closest('li').remove();
 
-getItem();
+	})
+	
+});
 
-/*Takes returned value from previous function and manipulates it*/
-function postItem() {
-	var item = $('#add-items').val();
-	var work = '<p class="full-width no-strikethrough">'+ xMark +item +checkMark + '</p>';
-	$('#list-area').prepend(work);
-	$('#add-items').val('');
-	$('#list-area p:first-child')
-}
+
+/* future ideas */
+/*remove all list ittems at once*/
+/*save list temporarily*/
+/*add an onclick function to js for saved */
+/*add onclick function to js for recipies */
