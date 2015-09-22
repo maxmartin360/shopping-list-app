654c8d8c05b471d3bb7125cf5ed98b06941a81b8
Max Martin <maxmartin360@gmail.com>
2015-09-21 22:38:24 -0400

    update

--- js/function.js
+++ js/function.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
 	$('#add-items').keydown(function(e) {
 		
 		if (e.which == 13) {
-			$('#list-area').prepend('<li class="item"><span id="cross-off"></span>'+ $('#add-items').val() + '<span id="delete"></span></li>')
-			$('#add-items').val('');
-		} else {
-			if ($('#list-area').val().length == 0 );
-			alert('please enter a value');
+			if ($('#add-items').val() != ''){
+				$('#list-area').prepend('<li class="item"><span id="cross-off"></span>'+ $('#add-items').val() + '<span id="delete"></span></li>')
+				$('#add-items').val('');
+			} else {
+				alert('Please enter a value');
+			}
+		
 		};
 
 	});
 //Change class when user clicks done button
 	$('ul').on('click','#cross-off', function() {
-		alert('this is being clicked')
-		$(this).closest('li').css({
-			'text-decoration': 'line-through',
-			'color':'#898989',
-		});
+		
+		$(this).closest('li').toggleClass('done');
+		
 		//$('#cross-off').css({
 		//'background-url''(http://i.imgur.com/dbRCAoT.png)',
 		//});
@@ -27,7 +27,7 @@ $(document).ready(function () {
 	});
 	//remove li when user hits x
 	$('ul').on('click','#delete', function() {
-		alert('you clicked delete')
+		//alert('you clicked delete')
 		$(this).closest('li').remove();
 
 	})
